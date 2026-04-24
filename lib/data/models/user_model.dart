9ffe84585161d9e29f51b86c5e061ca91f60bc39
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-24 14:19:17 +0700

    feat: deklarasi constructor UserModel

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,11 @@
+import 'package:sqlite/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.id,
+    required super.name,
+    required super.email,
+  });
+
+ 
+}
