e11160d81c34c81b69af54dabf2a8bfe3c0a9ae7
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-24 14:33:55 +0700

    feat: impelemntasi bloc dari tiap event yang udah dibuat

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqlite/bloc/user_event.dart';
+import 'package:sqlite/bloc/user_state.dart';
+import 'package:sqlite/domain/repository/user_repository.dart';
+
+class UserBloc  extends Bloc<UserEvent, UserState> {
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<LoadUsers>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final users = await repository.getAllUsers();
+        emit(UserLoaded(users));
+      } catch (e) {
+        emit(UserError('Gagal memuat data'));
+      }
+    });
+    on<AddUserEvent>((event, emit) async {
+      await repository.addUser(event.user);
+      add(LoadUsers());
+    });
+    on<UpdateUserEvent>((event, emit) async {
+      await repository.updateUser(event.user);
+      add(LoadUsers());
+    });
+    on<DeleteUserEvent>((event, emit) async {
+      await repository.deleteUser(event.id);
+      add(LoadUsers());
+    });
+  }
+}
