bd5937ba480b262e0a4f3ded5348ce93adb77da9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-24 14:23:10 +0700

    feat: buat database helper

---
+++ lib/helper/database_helper.dart
@@ -0,0 +1,12 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  
+}
