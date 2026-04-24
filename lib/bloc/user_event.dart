640ea84e21c9bedb807bb2eea9be2cb0e5310066
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-24 14:32:12 +0700

    feat: deklarasi event saat add user

--- lib/bloc/user_event.dart
+++ lib/bloc/user_event.dart
@@ -2,3 +2,7 @@ import 'package:sqlite/domain/entities/user_entity.dart';
 
 abstract class UserEvent {}
 class LoadUsers extends UserEvent {}
+class AddUserEvent extends UserEvent {
+  final UserEntity user;
+  AddUserEvent(this.user);
+}
