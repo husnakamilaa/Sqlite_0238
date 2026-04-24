70b5c20017ba34a85ca9b88999c69ffd20d46d77
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-24 16:28:40 +0700

    feat: deklarasi variable notelp dan alamat, input di constructornya juga

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -2,6 +2,8 @@ class UserEntity {
   final String id;
   final String name;
   final String email;
+  final String notelp;
+  final String alamat;
 
-  UserEntity({required this.id, required this.name, required this.email});
+  UserEntity({required this.id, required this.name, required this.email, required this.notelp, required this.alamat});
 }
